c52136b01a0805241f22a452c297008974d95ac4
Amir Sharapov <amir@amirfounder.com>
2022-05-02 08:03:51 -0500

    performance updates

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import re
+from os.path import exists
+
 import requests
 import bs4
 
@@ -7,12 +9,17 @@ CNN_MONEY_RSS = 'https://money.cnn.com/services/rss/'
 
 
 def get_cnn_rss_urls():
-    cnn_rss_html = requests.get(CNN_RSS).text
+    path = 'data/cnn_rss_html.html'
 
-    with open('data/cnn_rss_html.html', 'w') as f:
-        f.write(cnn_rss_html)
+    if not exists(path):
+        html = requests.get(CNN_MONEY_RSS).text
+        with open(path, 'w') as f:
+            f.write(html)
+    else:
+        with open(path, 'r') as f:
+            html = f.read()
 
-    soup = bs4.BeautifulSoup(cnn_rss_html, 'html.parser')
+    soup = bs4.BeautifulSoup(html, 'html.parser')
 
     tags = soup.find_all('a', {'href': re.compile(r'^http://rss.cnn.com/rss/')})
     urls = list(set([t.attrs['href'] for t in tags]))
@@ -22,19 +29,29 @@ def get_cnn_rss_urls():
 
 
 def get_cnn_money_rss_urls():
-    get_cnn_rss_urls()
+    path = 'data/cnn_money_rss_html.html'
 
-    cnn_money_rss_html = requests.get(CNN_MONEY_RSS).text
+    if not exists(path):
+        html = requests.get(CNN_MONEY_RSS).text
+        with open(path, 'w') as f:
+            f.write(html)
+    else:
+        with open(path, 'r') as f:
+            html = f.read()
 
-    with open('data/cnn_money_rss_html.html', 'w') as f:
-        f.write(cnn_money_rss_html)
-
-    soup = bs4.BeautifulSoup(cnn_money_rss_html, 'html.parser')
+    soup = bs4.BeautifulSoup(html, 'html.parser')
 
     tags = soup.find_all('a', {'href': re.compile(r'^http://rss.cnn.com/(rss/money_|cnnmoneymorningbuzz)')})
     urls = list(set([t.attrs['href'] for t in tags]))
-    topics = [u.removeprefix('http://rss.cnn.com/rss/money_').removeprefix('cnnmoneymorningbuzz').removesuffix('.rss') for u in urls]
+    topics = ['cnn_money_' + u.removeprefix('http://rss.cnn.com/rss/money_').removeprefix('cnnmoneymorningbuzz')
+        .removesuffix('.rss') for u in urls]
 
     return dict(zip(topics, urls))
 
-get_cnn_money_rss_urls()
+
+rss_feeds = {}
+
+rss_feeds.update(get_cnn_rss_urls())
+rss_feeds.update(get_cnn_money_rss_urls())
+
+print(rss_feeds)
