ba8a557a5117fcd2fafa41fc2f34c030178b6a64
Amir Sharapov <amir@amirfounder.com>
2022-05-05 18:28:47 -0500

    refactored modules and better logging.

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from abc import abstractmethod, ABC
 from datetime import datetime, timezone
+from enum import Enum
 
 
 class Model(ABC):
@@ -13,6 +14,8 @@ class Model(ABC):
 
     def __iter__(self):
         for k, v in self.__dict__.items():
+            if k.startswith('_'):
+                continue
             if isinstance(v, datetime):
                 v = v.isoformat()
             if isinstance(v, Model):
@@ -24,7 +27,6 @@ class Model(ABC):
                 for i, _v in enumerate(v):
                     v[i] = dict(v or {})
             yield k, v
-
 
 
 class Report(Model, ABC):
@@ -42,30 +44,48 @@ class Report(Model, ABC):
         self.has_been_attempted = True
         self.last_attempt_timestamp = datetime.now(timezone.utc)
 
-    def log_as_failed(self, error: str):
+    def fail(self, error: str):
         self.status = Report.FAILED
         self.error = error
 
-    def log_as_success(self):
+    def success(self):
         self.status = Report.SUCCESS
         self.error = None
+
+    def reset(self):
+        self.status = None
+        self.error = None
+        self.last_attempt_timestamp = None
+        self.has_been_attempted = False
+        self.additional_data = {}
+
+
+class ReportType(Enum):
+    scrape_urls = 'scrape_urls_v1'
+    extract_text = 'extract_text_v2'
 
 
 class IndexEntry(Model):
     def __init__(self, **kwargs):
+        self._index = kwargs['_index']
         self.reports: dict[str, Report] = kwargs['reports']
         self.url = kwargs['url']
         self.topic = kwargs['topic']
 
         self.scraped_html_path = kwargs['scraped_html_path']
-        self.extracted_text_v1_path = kwargs['extracted_text_v1_path']
-        self.extracted_text_v2_path = kwargs['extracted_text_v2_path']
+        self.extracted_text_path = kwargs['extracted_text_path']
+        self.preprocessed_text_path = kwargs['preprocessed_text_path'] if 'preprocessed_text_path' in kwargs else ''
 
     @classmethod
     def load(cls, obj: dict):
         for k, v in obj['reports'].items():
             obj['reports'][k] = Report.load(v)
         return super().load(obj)
+
+    def __getitem__(self, item):
+        # use dispatch from multipledispatch once this "switch" statements gets big enough
+        if isinstance(item, ReportType):
+            return self.reports[item.value]
 
 
 class ArticleText(Model):
